7c9842a7fcfe14e614f2aa23bdafd529fc0ca2e8
patilbp <bhagya22july@gmail.com>
2022-10-08 00:13:02 -0700

    added rolling average calculation in transformer

--- assignments/homework-3/RollingAvgTransform.py
+++ assignments/homework-3/RollingAvgTransform.py
@@ -1,10 +1,12 @@
 # Creating the Custom RDD Transformer
 # Reference: https://teaching.mrsharky.com/sdsu_fall_2020_lecture05.html#/7/11/5
 
+import pyspark.sql.functions as func
 from pyspark import keyword_only
 from pyspark.ml import Transformer
 from pyspark.ml.param.shared import HasInputCols, HasOutputCol
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.sql.window import Window
 
 
 class RollingBatAvgTransform(
@@ -29,7 +31,25 @@ class RollingBatAvgTransform(
     def _transform(self, dataset):
         input_cols = self.getInputCols()
         output_col = self.getOutputCol()
-        dataset = dataset.withColumn(output_col, dataset[input_cols[0]])
-        # Work-in-progress: Adding rolling days calculation here
+
+        # Calculate Rolling Average using Transformer
+        # Using UNIX_TIMESTAMP logic here, to convert date granularity 'day' to 'seconds'
+        def days(i):
+            return i * 86400
+
+        windowCalc = (
+            Window.partitionBy(dataset["batter"])
+            .orderBy(dataset["l_dt"].cast("long"))
+            .rangeBetween(days(-100), days(-1))
+        )
+
+        dataset = dataset.withColumn(
+            output_col,
+            func.coalesce(
+                func.sum(dataset[input_cols[0]]).over(windowCalc)
+                / func.sum(dataset[input_cols[1]]).over(windowCalc),
+                func.lit(0.0),
+            ),
+        )
 
         return dataset
